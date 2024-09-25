7a734267989261b95396f62e67d78f3e9f1271e6
Nihal-Aswal <160023111+Nihal-Aswal@users.noreply.github.com>
2024-09-25 22:24:48 +0530

    fibonacci

--- 54/fib.c
+++ 54/fib.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-int fib(int n){
-    if (n <= 1){
+#include <stdlib.h>
+
+long long fib(int n) {
+    if (n <= 1)
         return n;
-    }
-    return fib(n - 1) + fib(n - 2);
+    return fib(n-1) + fib(n-2);
 }
-int main() {
-    int n = 40;
-    printf("Fibonacci of  %d:\n", n);
-    printf("%d ", fib(n));
-    printf("\n");
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+    printf("Fibonacci number %d is %lld\n", n, fib(n));
     return 0;
-} 
+}
